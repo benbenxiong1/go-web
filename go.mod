76838f8904a4167315ff2457d9d200e510312e85
benbenxiong <1321335146@qq.com>
2022-02-07 17:24:27 +0800

    配置方案的实现

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module go-web
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/spf13/afero v1.8.1 // indirect
+	github.com/spf13/cast v1.4.1
+	github.com/spf13/viper v1.10.1
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
+	gopkg.in/ini.v1 v1.66.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-web/bootstrap"
+	bsConfig "go-web/config"
+	"go-web/pkg/config"
 )
 
+func init() {
+	bsConfig.Initialize()
+}
+
 func main()  {
+
+	var env string
+	flag.StringVar(&env,"env","","加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+
+	flag.Parse()
+	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	r := gin.New()
@@ -15,7 +28,7 @@ func main()  {
 	bootstrap.SetupRoute(r)
 
 	// 运行服务
-	err := r.Run(":8000")
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
